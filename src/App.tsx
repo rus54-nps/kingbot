2d897b40e65ee16fd96d2526c8421c8aa5e85e0f
rus54 <russ0054sergo@gmail.com>
2024-10-08 14:11:48 +0300

    k

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ function App() {
         <div className="fixed top-0 left-0 w-full px-4 pt-8 z-10 flex flex-col items-center text-white">
           <div className="w-full cursor-pointer">
             <div className="bg-[#1f1f1f] text-center py-2 rounded-xl">
-              <p className="text-lg">Join squad <Arrow size={18} className="ml-0 mb-1 inline-block" /></p>
+              <p className="text-lg">Join kingdom <Arrow size={18} className="ml-0 mb-1 inline-block" /></p>
             </div>
           </div>
           <div className="mt-12 text-5xl font-bold flex items-center">
