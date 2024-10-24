c3e7497f95ca898c131f5118f0f62ec3d6cd5682
rus54 <russ0054sergo@gmail.com>
2024-10-24 07:47:50 +0300

    version.011

--- src/Shop.tsx
+++ src/Shop.tsx
@@ -23,7 +23,7 @@ const Shop: React.FC<{
 }> = ({ points, setPoints, setCurrentPage, setEnergyRecoveryRate, setMaxEnergy, setPointsToAdd  }) => {
   const [items, setItems] = useState<ShopItem[]>([
     { id: 1, name: 'Тап lvl 1', price: 3000, image: item1, level: 1, regenerationRate: 1, nextPrice: 6000, description: 'Монеты за Тап: 1' },
-    { id: 2, name: 'Энергия lvl 1', price: 20, image: item2, level: 1, regenerationRate: 500, nextPrice: 50, description: 'Начальное количество энергии: 500' },
+    { id: 2, name: 'Энергия lvl 1', price: 2500, image: item2, level: 1, regenerationRate: 500, nextPrice: 5000, description: 'Начальное количество энергии: 500' },
     { id: 3, name: 'Реген lvl 1', price: 2500, image: item3, level: 1, regenerationRate: 1, nextPrice: 5000, description: 'Восстановление энергии: 1 в секунду' },
   ]);
 
