5a03150e8ba217fb64272569dff14419c9106239
rus54 <russ0054sergo@gmail.com>
2024-11-11 13:57:11 +0700

    version 0.060.1

--- src/Setting.tsx
+++ src/Setting.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Setting.css';
 
 interface SettingProps {
@@ -6,6 +6,33 @@ interface SettingProps {
 }
 
 const Setting: React.FC<SettingProps> = ({ onClose }) => {
+  const [isBackgroundSoundOn, setIsBackgroundSoundOn] = useState(false);
+  const [volume, setVolume] = useState(0.5); // начальный уровень громкости
+  const audio = new Audio('fon.mp3'); // Укажите путь к аудиофайлу
+
+  useEffect(() => {
+    // Обновляем громкость звука при изменении volume
+    audio.volume = volume;
+
+    // Включаем или останавливаем музыку в зависимости от состояния isBackgroundSoundOn
+    if (isBackgroundSoundOn) {
+      audio.loop = true; // Зацикливаем музыку
+      audio.play();
+    } else {
+      audio.pause();
+    }
+
+    return () => {
+      // Останавливаем и сбрасываем звук при размонтировании компонента
+      audio.pause();
+      audio.currentTime = 0;
+    };
+  }, [isBackgroundSoundOn, volume]);
+
+  const handleVolumeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setVolume(parseFloat(event.target.value));
+  };
+
   return (
     <div className="settings-overlay">
       <div className="settings-container">
@@ -16,8 +43,25 @@ const Setting: React.FC<SettingProps> = ({ onClose }) => {
         <div className="settings-content">
           <label>
             <span>Звук фона:</span>
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              checked={isBackgroundSoundOn}
+              onChange={() => setIsBackgroundSoundOn(!isBackgroundSoundOn)}
+            />
           </label>
+          {isBackgroundSoundOn && (
+            <label>
+              <span>Громкость:</span>
+              <input
+                type="range"
+                min="0"
+                max="1"
+                step="0.01"
+                value={volume}
+                onChange={handleVolumeChange}
+              />
+            </label>
+          )}
           <label>
             <span>Звук монеты:</span>
             <input type="checkbox" />
