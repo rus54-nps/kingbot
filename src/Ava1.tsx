b0fdfa00bbb06d569b5ee4f9ee2caf25fbe7ae32
rus54 <russ0054sergo@gmail.com>
2024-11-21 12:05:14 +0700

    0.064.2

--- src/Ava1.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Ava1 = () => {
-  return <img src="./goldh.png" alt="Avatar 1" className="avatar" />;
-};
-
-export default Ava1;
--- src/Ava2.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Ava1 = () => {
-  return <img src="/./dar.png" alt="Avatar 1" className="avatar" />;
-};
-
-export default Ava1;
