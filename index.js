eee507883349893e92085663abac91de851a890f
rus54 <russ0054sergo@gmail.com>
2024-11-28 21:17:20 +0700

    version 0.066

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+require('dotenv').config();
+
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '7470517993:AAHPNHF6rv7ebtXDPXXGC29tnekjjTeY2k0';
+// Загружаем токен из переменных окружения
+const token = process.env.BOT_TOKEN;
 
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
-  // send a message to the chat acknowledging receipt of their message
+  // отправляем сообщение в чат
   bot.sendMessage(chatId, 'Received your message');
 });
