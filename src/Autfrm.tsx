56751aafca6cc18d829ce53ca66cdcc4ef0d7e78
rus54 <russ0054sergo@gmail.com>
2024-12-18 05:32:46 +0700

    v

--- src/Autfrm.tsx
+++ src/Autfrm.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './Autfrm.css';
 import { bogy, goldh, dar, huntg, lackm, coin } from './images';
-import { useLanguage } from './LanguageContext';
 
 interface AutoFarmItem {
   id: number;
@@ -9,19 +8,11 @@ interface AutoFarmItem {
   price: number;
   image: string;
   level: number;
-  description: { ru: string; en: string };
+  description: string;
   incomePerHour: number;
   priceIncreaseFactor: number;
   incomeIncrease: number;
 }
-
-const itemNames = {
-  goldHands: { ru: 'Золотые Руки', en: 'Golden Hands' },
-  luckyCoin: { ru: 'Счастливая Монета', en: 'Lucky Coin' },
-  richHarvest: { ru: 'Богатый Урожай', en: 'Rich Harvest' },
-  giftOfFate: { ru: 'Дар Судьбы', en: 'Gift of Fate' },
-  treasureSeeker: { ru: 'Искатель Сокровищ', en: 'Treasure Seeker' },
-};
 
 const AutoFarm: React.FC<{
   points: number;
@@ -30,17 +21,15 @@ const AutoFarm: React.FC<{
 }> = ({ points, setPoints, setCurrentPage }) => {
   // Инициализация `items` из `localStorage` или начальных значений
   const initialItems: AutoFarmItem[] = [
-    { id: 1, name: 'goldHands', price: 8000, image: goldh, level: 0, description: { ru: '0 монет в час', en: '0 coins per hour' }, incomePerHour: 4000, priceIncreaseFactor: 1.4, incomeIncrease: 150 },
-    { id: 2, name: 'luckyCoin', price: 10000, image: lackm, level: 0, description: { ru: '0 монет в час', en: '0 coins per hour' }, incomePerHour: 5000, priceIncreaseFactor: 1.45, incomeIncrease: 80 },
-    { id: 3, name: 'richHarvest', price: 12000, image: bogy, level: 0, description: { ru: '0 монет в час', en: '0 coins per hour' }, incomePerHour: 6000, priceIncreaseFactor: 1.5, incomeIncrease: 200 },
-    { id: 4, name: 'giftOfFate', price: 15000, image: dar, level: 0, description: { ru: '0 монет в час', en: '0 coins per hour' }, incomePerHour: 7000, priceIncreaseFactor: 1.5, incomeIncrease: 300 },
-    { id: 5, name: 'treasureSeeker', price: 18800, image: huntg, level: 0, description: { ru: '0 монет в час', en: '0 coins per hour' }, incomePerHour: 8000, priceIncreaseFactor: 1.55, incomeIncrease: 350 },
+    { id: 1, name: 'Золотые Руки', price: 8000, image: goldh, level: 0, description: '0 монет в час', incomePerHour: 4000, priceIncreaseFactor: 1.4, incomeIncrease: 150 },
+    { id: 2, name: 'Счастливая Монета', price: 10000, image: lackm, level: 0, description: '0 монет в час', incomePerHour: 5000, priceIncreaseFactor: 1.45, incomeIncrease: 80 },
+    { id: 3, name: 'Богатый Урожай', price: 12000, image: bogy, level: 0, description: '0 монет в час', incomePerHour: 6000, priceIncreaseFactor: 1.5, incomeIncrease: 200 },
+    { id: 4, name: 'Дар Судьбы', price: 15000, image: dar, level: 0, description: '0 монет в час', incomePerHour: 7000, priceIncreaseFactor: 1.5, incomeIncrease: 300 },
+    { id: 5, name: 'Искатель Сокровищ', price: 18800, image: huntg, level: 0, description: '0 монет в час', incomePerHour: 8000, priceIncreaseFactor: 1.55, incomeIncrease: 350 },
   ];
 
   const savedItems = localStorage.getItem('autoFarmItems');
   const [items, setItems] = useState<AutoFarmItem[]>(savedItems ? JSON.parse(savedItems) : initialItems);
-
-   const { language } = useLanguage();
 
   useEffect(() => {
     const lastExitTime = localStorage.getItem('lastExitTime');
@@ -56,7 +45,7 @@ const AutoFarm: React.FC<{
       });
 
       const passiveIncome = totalIncomePerHour * hoursToAdd;
-      setPoints(prevPoints => prevPoints + Math.trunc(passiveIncome));
+      setPoints(prevPoints => prevPoints + Math.floor(passiveIncome));
     }
   }, [items, setPoints]);
 
@@ -91,10 +80,7 @@ const handlePurchase = (itemId: number) => {
           level: newLevel,
           price: Math.round(newPrice), // Обновляем цену
           incomePerHour: newIncome,
-          description: {
-            ru: `${newIncome} монет в час`,
-            en: `${newIncome} coins per hour`,
-          }, // Обновляем описание
+          description: `${newIncome} монет в час.`, // Обновляем описание
         };
       }
       return item;
@@ -103,23 +89,23 @@ const handlePurchase = (itemId: number) => {
     setItems(updatedItems);
     localStorage.setItem('autoFarmItems', JSON.stringify(updatedItems)); // Сохраняем обновленные данные
     setPoints(points - itemToPurchase.price);
-    alert(`${language === 'ru' ? 'Вы купили' : 'You bought'} ${itemToPurchase.name}! ${language === 'ru' ? 'Новый уровень' : 'New level'}: ${itemToPurchase.level + 1}`);
+    alert(`Вы купили ${itemToPurchase.name}! Новый уровень: ${itemToPurchase.level + 1}`);
   } else {
-    alert(language === 'ru' ? 'Недостаточно очков для покупки!' : 'Not enough points to buy!');
+    alert('Недостаточно очков для покупки!');
   }
 };
 
   return (
     <div className="Aut autofarm-overlay">
-      <h2 className="autofarm-title">{language === 'ru' ? 'Автофарм' : 'Autofarm'}</h2>
+      <h2 className="autofarm-title">Автофарм</h2>
       <div className="autofarm-balance">
         <img src={coin} alt="Coin" width={20} height={20} />
         <span>{Math.floor(points)}</span>
       </div>
 
       <div className="autofarm-passive-income">
-        <div className="autofarm-passive-title">{language === 'ru' ? 'Пассивный доход' : 'Passive income'}</div>
-        <p className="autofarm-passive-text">{language === 'ru' ? 'После того как ты закрыл игру, пассивный доход копится 3 часа.' : 'After you close the game, passive income accumulates for 3 hours.'}</p>
+        <div className="autofarm-passive-title">Пассивный доход</div>
+        <p className="autofarm-passive-text">После того как ты закрыл игру, пассивный доход копится 3 часа.</p>
       </div>
       <div className="autofarm-items-container">
         <ul className="autofarm-items">
@@ -130,9 +116,8 @@ const handlePurchase = (itemId: number) => {
                 <div className="autofarm-item-level">Lvl {item.level}</div>
               </div>
               <div className="autofarm-item-info">
-              <h3>{itemNames[item.name as keyof typeof itemNames][language]}</h3>
-
-              <p>{item.description[language]}</p>
+                <h3>{item.name}</h3>
+                <p>{item.description}</p>
                 <button className="buy-button" onClick={() => handlePurchase(item.id)}>
                   <img src={coin} alt="Coin" width={16} height={16} />
                   {item.price}
@@ -142,7 +127,7 @@ const handlePurchase = (itemId: number) => {
           ))}
         </ul>
       </div>
-      <button className="autofarm-back-button" onClick={() => setCurrentPage('home')}>{language === 'ru' ? 'Назад' : 'back'}</button>
+      <button className="autofarm-back-button" onClick={() => setCurrentPage('home')}>Назад</button>
     </div>
   );
 };
